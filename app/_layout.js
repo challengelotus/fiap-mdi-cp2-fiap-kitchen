9fdfe0bed1998b5836161c578a36407a58685861
pedrinhodps <pehenmendes@gmail.com>
2026-03-11 22:50:16 -0300

    criação dos arquivos das telas e definição dos routers

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,16 @@
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="cardapio" options={{ headerShown: false }} />
+      <Stack.Screen name="salgados" options={{ headerShown: false }} />
+      <Stack.Screen name="bebidas" options={{ headerShown: false }} />
+      <Stack.Screen name="sobremesas" options={{ headerShown: false }} />
+      <Stack.Screen name="carrinho" options={{ headerShown: false }} />
+      <Stack.Screen name="pagamento" options={{ headerShown: false }} />
+      <Stack.Screen name="codigoPedido" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
